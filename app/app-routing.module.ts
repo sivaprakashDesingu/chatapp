3d65de3d2c7816d9d9f866bb74234d7fd92bc486
Sivaprakash Desingu <opensourcesivaprakash@gmail.com>
2018-07-13 18:50:50 +0530

    Add files via upload
    july 13th update

--- app/app-routing.module.ts
+++ app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { ForgotComponent } from './forgot/forgot.component';
 import { RegisterComponent } from './register/register.component';
 import { ChatboxComponent } from './chatbox/chatbox.component';
-
+import { NotfoundComponent } from './notfound/notfound.component';
 
 const routes: Routes = [
 
@@ -14,7 +14,8 @@ const routes: Routes = [
   { path: 'frgt', component: ForgotComponent },
   { path: 'rgstr', component: RegisterComponent },
   { path: 'dash/chat/:rid', component: ChatboxComponent },
-
+  {path: '404', component: NotfoundComponent},
+  {path: '**', redirectTo: '/404'}
 
 
 ];
