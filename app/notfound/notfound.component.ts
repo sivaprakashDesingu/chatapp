dd0c508b3b65c62a0fdf0cfe8fda40ec3d00c795
Sivaprakash Desingu <10977339+sivaprakashDesingu@users.noreply.github.com>
2018-08-12 19:00:06 +0530

    just uplaod demo

--- app/notfound/notfound.component.ts
+++ app/notfound/notfound.component.ts
@@ -20,34 +20,38 @@ export class NotfoundComponent implements OnInit {
     const formData: any = new FormData();
     const files: Array<File> = this.filesToUpload;
     console.log(files);
-
-    for(let i =0; i < files.length; i++){
-        formData.append("uploads[]", files[i], files[i]['name']);
-    }
-    console.log('form data variable :   '+ formData.toString());
-    this.http.post('http://localhost:3000/upload', formData)
+    if (files[0].type == "image/jpg" || files[0].type == "image/png" || files[0].type == "image/jpeg") {
+      for (let i = 0; i < files.length; i++) {
+        formData.append("uploads[]", files[i], 'sp_profile.jpg');
+      }
+      console.log('form data variable :   ' + formData.toString());
+      this.http.post('http://localhost:3000/upload', formData)
         //.map(files => files.json())
         .subscribe(files => console.log('files', files))
-}
+    }else{
+       console.log("Please select an Image file...")
+    }
+  }
 
-fileChangeEvent(fileInput: any) {
+  fileChangeEvent(fileInput: any) {
     this.filesToUpload = <Array<File>>fileInput.target.files;
+    console.log(this.filesToUpload[0].type);
     //this.product.photo = fileInput.target.files[0]['name'];
 
-    
-}
-// localUrl: any[];
-// showPreviewImage(event: any) {
-//   if (event.target.files && event.target.files[0]) {
-//       var reader = new FileReader();
-//       reader.onload = (event: any) => {
-//           this.localUrl = event.target.result;
-//           console.log(this.localUrl);
-//       }
-//       reader.readAsDataURL(event.target.files[0]);
-//   }
-//   //console.log(this.localUrl);
-// }
+
+  }
+  // localUrl: any[];
+  // showPreviewImage(event: any) {
+  //   if (event.target.files && event.target.files[0]) {
+  //       var reader = new FileReader();
+  //       reader.onload = (event: any) => {
+  //           this.localUrl = event.target.result;
+  //           console.log(this.localUrl);
+  //       }
+  //       reader.readAsDataURL(event.target.files[0]);
+  //   }
+  //   //console.log(this.localUrl);
+  // }
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
